d015bb1e73d624919ad3e18503caba8855559107
Niraporn Kovitaya <bam_28@hotmail.com>
2022-07-02 04:52:29 +0700

    Fix package.json

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devServer: {
+    // host: "localhost:8080", //frontend host name
+    allowedHosts: "all",
+    port: 8080, //frontend port
+    https: false, //no ssl
+    proxy: {
+      "/auth": {
+        target: "http://localhost:8082",
+      },
+    },
+    headers: {
+      // typical header setting
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":  "X-Request-With, content-type, Authorization",
+    },
+  },
+};
