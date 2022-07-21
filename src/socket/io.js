275303c130c826863d422e8fcb829e4c7a61a1e5
Niraporn Kovitaya <bam_28@hotmail.com>
2022-07-21 20:10:07 +0700

    re-build

--- src/socket/io.js
+++ src/socket/io.js
@@ -2,4 +2,5 @@ import io from "socket.io-client";
 
 const socket = io.connect('/socket.io', {'timeout': 50000, 'connect_timeout': 50000})
 await new Promise(r => setTimeout(r, 20000));
+
 export {socket}
